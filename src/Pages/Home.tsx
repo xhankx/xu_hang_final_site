11c9df3b896cb92948ac06d7b69180d397ea7621
xhankx <hxu7@academic.rrc.ca>
2024-12-15 13:11:33 -0600

    Update Home.tsx

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HeroImage from 'xu_hang_ui_garden_build_checks';
+
+const Home = () => (
+  <div>
+    <HeroImage src="https://via.placeholder.com/1200x400" alt="Hero Banner" />
+    <h1>Welcome to My Portfolio</h1>
+  </div>
+);
+
+export default Home;
