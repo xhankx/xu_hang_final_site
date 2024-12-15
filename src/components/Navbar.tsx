fd26a572132dcf533dd579e8bd960cbd96306ffe
xhankx <hxu7@academic.rrc.ca>
2024-12-15 13:10:55 -0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,6 @@ const Navbar = () => (
       <li><Link to="/about">About</Link></li>
       <li><Link to="/projects">Projects</Link></li>
       <li><Link to="/skills">Skills</Link></li>
-      <li><Link to="/work-summary">Work Summary</Link></li>
       <li><Link to="/contact">Contact</Link></li>
     </ul>
   </nav>
